bc76f641b7f4a8538f2c51629f48ee327711b12c
Kaija Gahm (cary) <kaijagahm@Kaijas-MacBook-Pro.local>
2021-01-21 10:26:28 -0500

    script to bin DOC data

---
+++ code/DOC_binning.R
@@ -0,0 +1,24 @@
+# Script to classify lakes into DOC categories
+# Written by Kaija Gahm, January 2021
+
+# Load packages -----------------------------------------------------------
+library(here)
+library(dplyr)
+
+# Load lake info ----------------------------------------------------------
+l <- read.csv(here("data", "outputs", "Lake_Info_2020wBasins.csv"))
+
+# Assign DOC categories ----------------------------------------------------
+# According to Chelsea, the categories we will use are: 
+# 1. For low/high: 10mg/L is the cutoff
+# 2. For bins, they're 0-5, 5-10, 10-15, 15-20, 20-25, etc.
+
+l <- l %>%
+  mutate(DOClevel = case_when(DOC > 10 ~ "high",
+                              TRUE ~ "low"),
+         DOCbin = case_when(DOC < 10 ~ "0-10",
+                            DOC > 10 & DOC < 15 ~ "10-15",
+                            DOC > 15 & DOC < 25 ~ "15-25"))
+
+# Write out ---------------------------------------------------------------
+write.csv(l, here("data", "outputs", "lakeInfo_wBins.csv"), row.names = F)
