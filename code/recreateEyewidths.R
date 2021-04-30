fc8627998cbde348cbffd1922c69e2e9bde424f7
Kaija Gahm (cary) <kaijagahm@Kaijas-MacBook-Pro.local>
2021-04-30 11:46:16 -0400

    used std lengths from FM; removed calculation of std lengths

--- code/recreateEyewidths.R
+++ code/recreateEyewidths.R
@@ -25,9 +25,10 @@ lakeInfo <- read.csv(here("data", "outputs", "Lake_Info_2020wBasins.csv"))
 
 # Initialize recreated df -------------------------------------------------
 ewR <- fm %>%
+  filter(replicate == "NA") %>%
   select(imageFile, parameter, parameterValue) %>%
   rename("fishID" = imageFile) %>%
-  filter(parameter == "eyeWidth") %>%
+  filter(parameter %in% c("eyeWidth", "stdLength")) %>%
   pivot_wider(id_cols = fishID, names_from = "parameter", values_from = "parameterValue")
 
 # Are all the fish represented?
@@ -69,45 +70,17 @@ ewR <- ewR %>%
               select(lakeName, DOC, basin),
             by = c("lakeID" = "lakeName"))
 
-# Not adding capture method because it doesn't get used in analyses.
-
-# Calculate fish standard length ------------------------------------------
-# XXX Soon, will be able to pull this directly from FISH_MORPHOMETRICS
-# Standard length is the distance between landmarks 1 and 7
-fsl <- fm %>%
-  filter(parameter %in% c("X7", "Y7", "X1", "Y1")) %>%
-  select(imageFile, parameter, parameterValue) %>%
-  group_by(imageFile, parameter) %>% # because standard length was calculated based on just the first measurement, we group by imageFile and parameter and select the first row.
-  slice(1) %>%
-  ungroup() %>%
-  pivot_wider(names_from = parameter, values_from = parameterValue) %>%
-  rename("fishID" = imageFile) %>%
-  mutate(fishStdLength = sqrt((X7-X1)^2+(Y7-Y1)^2)) %>%
-  select(fishID, fishStdLength) %>%
-  filter(fishID %in% ewR$fishID)
-
-## join
-ewR <- ewR %>%
-  left_join(fsl, by = "fishID")
-
-## Check
-ewR$fishStdLength %>% head(10)
-ew$fishStdLength %>% head(10)
-
-# ok, these are the same to 2 decimal places. So not *quite* exactly the same.
-
 # Size-standardize the eye widths -----------------------------------------
 # First, let's compute the mean fishStdLength across all fish
-meanFishSize <- mean(ewR$fishStdLength)
+meanFishSize <- mean(ewR$stdLength)
 
 # Now, we make three different models and compare them to determine which coefficient to use. See sizeStandardizationNotes.docx for more details on this approach.
-moda <- lm(log(eyeWidth) ~ log(fishStdLength), data = ewR)
-modb <- lm(log(eyeWidth) ~ log(fishStdLength)+lakeID, data = ewR)
-modc <- lm(log(eyeWidth) ~ log(fishStdLength)*lakeID, data = ewR)
-summary(modc) # we note that there is an interaction effect for CR, LT, and MS.
+moda <- lm(log(eyeWidth) ~ log(stdLength), data = ewR)
+modb <- lm(log(eyeWidth) ~ log(stdLength)+lakeID, data = ewR)
+modc <- lm(log(eyeWidth) ~ log(stdLength)*lakeID, data = ewR)
 
 # Compare models to determine which coefficient to use
-anova(modc, modb) # model c is significantly better than model b
+anova(modc, modb) # model c is significantly better than model b (p = 0.00229)
 
 # We will still use the coefficient from model b, as is standard practice (see sizeStandardizationNotes.docx)
 coef <- coef(modb)[2] %>% unname() # pull the "Estimate" parameter from the model
@@ -116,7 +89,7 @@ coef #  0.6266465
 # Now compute size-standardized eye width, adding the column to `ewR`
 ## We're using the Kaeuffer version of the formula here (see notes doc)
 ewR <- ewR %>%
-  mutate(eyewidth.ss = eyeWidth*(meanFishSize/fishStdLength)^coef)
+  mutate(eyewidth.ss = eyeWidth*(meanFishSize/stdLength)^coef)
 
 ## check it against the original
 head(ewR$eyewidth.ss)
@@ -126,4 +99,3 @@ head(ew$sizeStandardize) # these are very close but slightly different. I think 
 
 # Write out the data ------------------------------------------------------
 write.csv(ewR, file = here("data", "outputs", "eyewidthsFINAL.csv"), row.names = F)
-
